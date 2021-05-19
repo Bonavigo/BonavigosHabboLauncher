a9aa95935916fa3cc705cf3e072d7ea19c0e74db
Bruno Bonavigo <b.bonavigo@gmail.com>
2021-05-19 19:28:33 -0300

    Adicionar arquivos de projeto.

---
+++ BonavigoHabboLauncher/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace BonavigoHabboLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Launch Habbo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Tag = "";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(90, 109);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(91, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "New instance";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(350, 300);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ BonavigoHabboLauncher/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Xml;
+using System.Windows.Forms;
+
+namespace BonavigoHabboLauncher
+{
+    public partial class Form1 : Form
+    {
+        protected String hotelCode;
+        protected String logCode;
+        protected bool checkboxNewInstance;
+        protected String executingPath = AppDomain.CurrentDomain.BaseDirectory;
+        protected String gamePath;
+        protected String XMLPath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (getPath())
+            {
+                if (String.IsNullOrEmpty(this.logCode))
+                {
+                    MessageBox.Show("Você precisa inserir um código de login!", "Erro!");
+                }
+                else
+                {
+                    openGame();
+                }
+            }
+        }
+        
+        private bool getPath()
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                string[] path = { this.executingPath, "config.xml" };
+                string fullPath = Path.Combine(path);
+                doc.Load(fullPath);
+                var elementos = doc.SelectNodes("//*");
+                for (int i = 0; i < elementos.Count; i++)
+                {
+                    var elemento = elementos[i];
+                    if (elemento.Name == "path")
+                    {
+                        this.gamePath = elemento.InnerText;
+                    }
+                }
+                string[] pathXML = { this.gamePath, "META-INF", "AIR", "application.xml" };
+                string fullPathXML = Path.Combine(pathXML);
+                this.XMLPath = fullPathXML;
+                return true;
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+                return false;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string[] subs = textBox1.Text.Split(new[] { '.' }, 2);
+            if (subs.Count() == 2)
+            {
+                this.hotelCode = subs[0];
+                this.logCode = subs[1];
+            } else
+            {
+                this.hotelCode = null;
+                this.logCode = null;
+            }
+        }
+
+        private void openGame()
+        {
+            if (this.checkboxNewInstance)
+            {
+                this.newIDApplictionXML();
+            }
+            Process.Start(new ProcessStartInfo("Habbo.exe")
+            {
+                WorkingDirectory = this.gamePath,
+                Arguments = $"-server {this.hotelCode} -ticket \"{this.logCode}\""
+            });
+            if (this.checkboxNewInstance)
+            {
+                this.resetApplicationXML();
+            }
+        }
+
+        private void newIDApplictionXML()
+        {
+            try {
+                string new_id = $"com.sulake.habboair.{this.hotelCode}" + DateTimeOffset.Now.ToUnixTimeSeconds();
+                XmlDocument doc = new XmlDocument();
+                doc.Load(this.XMLPath);
+                var elementos = doc.SelectNodes("//*");
+                for (int i = 0; i < elementos.Count; i++)
+                {
+                    var elemento = elementos[i];
+                    if (elemento.Name == "id")
+                    {
+                        elemento.InnerText = new_id;
+                    }
+                }
+                doc.Save(this.XMLPath);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void resetApplicationXML()
+        {
+            try
+            {
+                string new_id = $"com.sulake.habboair.{this.hotelCode}";
+                XmlDocument doc = new XmlDocument();
+                doc.Load(this.XMLPath);
+                var elementos = doc.SelectNodes("//*");
+                for (int i = 0; i < elementos.Count; i++)
+                {
+                    var elemento = elementos[i];
+                    if (elemento.Name == "id")
+                    {
+                        elemento.InnerText = new_id;
+                    }
+                }
+                doc.Save(this.XMLPath);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.checkboxNewInstance = checkBox1.Checked;
+        }
+    }
+}
